859b97b17d5ae97db3cd96b8d03b9ee017e68724
Aritana Noara Costa Santos <aritanacs@ciandt.com>
2022-03-14 16:32:57 -0300

    ListMaker: Done with recycler view

---
+++ ListMaker/app/src/main/java/com/aritana/listmaker/ui/main/ListSelectionViewHolder.kt
@@ -0,0 +1,7 @@
+package com.aritana.listmaker.ui.main
+
+import androidx.recyclerview.widget.RecyclerView
+import com.aritana.listmaker.databinding.ListSelectionViewHolderBinding
+
+class ListSelectionViewHolder(val binding: ListSelectionViewHolderBinding) : RecyclerView.ViewHolder(binding.root) {
+}
