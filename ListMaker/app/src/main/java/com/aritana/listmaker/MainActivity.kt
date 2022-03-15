5a4d0ab796820295d566f61ac2594d1e0e4cf639
Aritana Noara Costa Santos <aritanacs@ciandt.com>
2022-03-15 00:03:24 -0300

    ListMaker: Adding a Dialog

--- ListMaker/app/src/main/java/com/aritana/listmaker/MainActivity.kt
+++ ListMaker/app/src/main/java/com/aritana/listmaker/MainActivity.kt
@@ -2,17 +2,57 @@ package com.aritana.listmaker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.InputType
+import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
+import com.aritana.listmaker.databinding.MainActivityBinding
 import com.aritana.listmaker.ui.main.MainFragment
+
+private lateinit var binding: MainActivityBinding
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
+
+        binding = MainActivityBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, MainFragment.newInstance())
                 .commitNow()
         }
+
+        binding.fabButton.setOnClickListener{
+            showCreateListDialog()
+        }
     }
+
+
+    private fun showCreateListDialog() {
+        // 1
+        val dialogTitle = getString(R.string.name_of_list)
+        val positiveButtonTitle = getString(R.string.create_list)
+
+        // 2
+        val builder = AlertDialog.Builder(this)
+        val listTitleEditText = EditText(this)
+        listTitleEditText.inputType = InputType.TYPE_CLASS_TEXT
+
+        builder.setTitle(dialogTitle)
+        builder.setView(listTitleEditText)
+
+        // 3
+        builder.setPositiveButton(positiveButtonTitle) { dialog, _ ->
+            dialog.dismiss()
+        }
+
+        // 4
+        builder.create().show()
+    }
+
+
+
 }
